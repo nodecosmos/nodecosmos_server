5e53f1bdfdd362a4a91c71ba2c7a84091b62d2a6
Goran <goran.brkuljan@gmail.com>
2023-04-18 15:31:53 +0200

    add post model

---
+++ nodecosmos/src/models/post.rs
@@ -0,0 +1,17 @@
+use super::udts::Address;
+use charybdis::prelude::*;
+use scylla::_macro_internal::FromRowError;
+
+#[partial_model_generator]
+#[charybdis_model(table_name = "posts",
+                  partition_keys = ["created_at_day"],
+                  clustering_keys = ["title"],
+                  secondary_indexes = [])]
+pub struct Post {
+    pub id: Uuid,
+    pub created_at_day: Date,
+    pub title: Text,
+    pub description: Text,
+    pub created_at: Timestamp,
+    pub updated_at: Timestamp,
+}
