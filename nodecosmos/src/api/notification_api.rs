4dff6937939fc5574dfc523f188e8695709a15f6
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-06-28 16:22:44 +0200

    update: support fetching only new notifications

--- nodecosmos/src/api/notification_api.rs
+++ nodecosmos/src/api/notification_api.rs
@@ -13,30 +13,43 @@ use serde_json::json;
 #[derive(Deserialize)]
 pub struct PagedQuery {
     pub paging_state: Option<String>,
+    pub new: Option<bool>,
 }
 
 #[get("/")]
 pub async fn get_notifications(data: RequestData, q: web::Query<PagedQuery>) -> Response {
-    let query = Notification::find_by_partition_key_value_paged((data.current_user.id,));
-
     let paging_state = if let Some(q) = q.paging_state.as_ref() {
         Some(scylla::Bytes::from(URL_SAFE.decode(&q)?))
     } else {
         None
     };
+    let query = Notification::find_by_partition_key_value_paged((data.current_user.id,));
 
-    let (notification_res, paging_state) = query
-        .page_size(PAGE_SIZE)
-        .paging_state(paging_state)
-        .execute(data.db_session())
-        .await?;
+    let (notifications, paging_state) = if q.new.unwrap_or(false) {
+        let notifications = Notification::find_by_user_id_and_seen(data.current_user.id, false)
+            .execute(data.db_session())
+            .await?
+            .try_collect()
+            .await?;
 
-    let notifications: Result<Vec<Notification>, CharybdisError> = notification_res.collect();
-    let paging_state = paging_state.map(|p| URL_SAFE.encode(&p));
+        (notifications, paging_state)
+    } else {
+        let (notification_res, paging_state) = query
+            .page_size(PAGE_SIZE)
+            .paging_state(paging_state)
+            .execute(data.db_session())
+            .await?;
+
+        let notifications: Result<Vec<Notification>, CharybdisError> = notification_res.collect();
+
+        (notifications?, paging_state)
+    };
+
+    let paging_state = paging_state.map(|ps| URL_SAFE.encode(&ps));
 
     Ok(HttpResponse::Ok().json(json!({
-        "notifications": notifications?,
-        "paging_state": paging_state,
+        "notifications": notifications,
+        "pagingState": paging_state,
     })))
 }
 
