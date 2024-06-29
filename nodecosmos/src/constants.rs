02d7736ba82fcccf686f5317192c0fe4a0980e2f
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-06-29 21:56:01 +0200

    updates

--- nodecosmos/src/constants.rs
+++ nodecosmos/src/constants.rs
@@ -1,3 +1,5 @@
 pub const MAX_PARALLEL_REQUESTS: usize = 25;
 pub const BATCH_CHUNK_SIZE: usize = 100;
 pub const PAGE_SIZE: i32 = 51;
+pub const BLACKLIST_USERNAMES: [&str; 6] = ["auth", "404", "reset_password", "node", "nodes", "invitations"];
+// use rustrict for innapropriate words
