9bf32555208b51cf2ca1389847af39e668b40012
Goran Brkuljan <goran.brkuljan@gmail.com>
2024-07-03 13:44:06 +0200

    update: prevent inappropriate usernames

--- nodecosmos/src/constants.rs
+++ nodecosmos/src/constants.rs
@@ -1,5 +1,31 @@
 pub const MAX_PARALLEL_REQUESTS: usize = 25;
 pub const BATCH_CHUNK_SIZE: usize = 100;
 pub const PAGE_SIZE: i32 = 51;
-pub const BLACKLIST_USERNAMES: [&str; 6] = ["auth", "404", "reset_password", "node", "nodes", "invitations"];
-// use rustrict for innapropriate words
+
+pub const BLACKLIST_USERNAMES_SORTED: [&str; 25] = [
+    "404",
+    "about",
+    "admin",
+    "administrator",
+    "auth",
+    "blacklist",
+    "contact",
+    "doc",
+    "home",
+    "invitations",
+    "login",
+    "mine",
+    "node",
+    "nodecosmos",
+    "nodes",
+    "nodescosmos",
+    "password",
+    "reset_password",
+    "social",
+    "socials",
+    "support",
+    "test",
+    "tester",
+    "testing",
+    "username",
+];
